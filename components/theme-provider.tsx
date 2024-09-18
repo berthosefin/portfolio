3518e4ad2e265e91d6bba42a6694b4b2a8584ad0
Berthose Fin <berthosefin@gmail.com>
2024-09-18 16:46:52 +0300

    Refact theme provider

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,9 +1,17 @@
 'use client'
 
-import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { type ThemeProviderProps } from 'next-themes/dist/types'
+import * as React from 'react'
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <NextThemesProvider
+      enableSystem
+      attribute='class'
+      defaultTheme='system'
+      disableTransitionOnChange
+    >
+      {children}
+    </NextThemesProvider>
+  )
 }
