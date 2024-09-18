3368b24838408360e185d873c88901b0fcfe4942
Berthose Fin <berthosefin@gmail.com>
2024-09-18 18:47:19 +0300

    Intro

---
+++ components/intro.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import authorImage from '@/public/images/thos.jpg'
+
+export default function Intro() {
+  return (
+    <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
+      <div className='mt-2 flex-1 md:mt-0'>
+        <h1 className='text-3xl font-bold'>Hey, I&#39;m Thos.</h1>
+        <p className='mt-3 text-justify font-light text-muted-foreground'>
+          I&#39;m a self-taught fullstack developer from Madagascar, passionate
+          about web development, open source, and Linux. My journey has been
+          driven by curiosity and a love for learning, exploring modern
+          technologies like React, Next.js, NestJS, and containerization, while
+          personalizing my workflow through Linux.
+        </p>
+      </div>
+      <div className='relative'>
+        <Image
+          className='flex-1 rounded-lg grayscale'
+          src={authorImage}
+          alt='Berthose Fin'
+          width={175}
+          height={175}
+          priority
+        />
+      </div>
+    </section>
+  )
+}
