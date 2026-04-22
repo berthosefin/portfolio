b5f814f515058e03693f23070827468d31bb66f2
Berthose Fin <berthosefin@gmail.com>
2026-04-22 22:20:00 +0300

    fix: disable secure transport for email configuration in nodemailer actions

--- lib/actions.ts
+++ lib/actions.ts
@@ -9,7 +9,7 @@ type ContactFormInputs = z.infer<typeof ContactFormSchema>
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
